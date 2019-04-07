2b8ddc2b301bda7c74f74ee8156aa43e7e2956fd
Jacek Kościesza <jacek.kosciesza@gmail.com>
2019-04-07 17:12:10 +0200

    HttpClient unit test

---
+++ backend/Gateway/Tests/Gateway.Tests.Unit/Services/WalletServiceShould.cs
@@ -0,0 +1,41 @@
+﻿using Gateway.API.Services;
+using MaxKagamine.Moq.HttpClient;
+using Moq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Gateway.Tests.Unit.Services
+{
+    public class WalletServiceShould
+    {
+        [Fact]
+        public async Task CallWalletsApi()
+        {
+            // Arrange
+            var httpMessageHandler = new Mock<HttpMessageHandler>();
+            var httpClient = httpMessageHandler.CreateClient();
+            var list = new List<Models.Wallet>
+            {
+                new Models.Wallet
+                {
+                    Id = 1,
+                    Name = "Private",
+                    Balance = 999.95m
+                }
+            };
+            httpMessageHandler.SetupRequest(HttpMethod.Get, "https://localhost:5003/api/wallets")
+            .ReturnsResponse(JsonConvert.SerializeObject(list), "application/json");
+            var walletsService = new WalletService(httpClient);
+
+            // Act
+            var wallets = await walletsService.GetWallets();
+
+            // Assert
+            httpMessageHandler.VerifyAnyRequest(Times.Once());
+            // Assert.Equal(list, wallets); // TODO: check why it doesn't work
+        }
+    }
+}
